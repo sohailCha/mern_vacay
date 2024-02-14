8898e796c06f61f9faa83189854c779d7187ba72
Sohail Chauhan <chauhansohail14@gmail.com>
2024-02-14 16:29:28 -0600

    Added hotel vies and e2e tests

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -46,3 +46,19 @@ test('should allow user to add hotel', async ({ page }) => {
 	await page.getByRole('button', { name: 'Save' }).click()
 	await expect(page.getByText('Hotel Saved')).toBeVisible({ timeout: 15000 })
 })
+
+test('should display hotels', async ({ page }) => {
+	await page.goto(`${UI_URL}/my-hotels`)
+
+	await expect(page.getByText('Test Hotel')).toBeVisible()
+	await expect(page.getByText('Test Description')).toBeVisible()
+
+	await expect(page.getByText('Test City, Test Country')).toBeVisible()
+	await expect(page.getByText('Budget')).toBeVisible()
+	await expect(page.getByText('$100 per night')).toBeVisible()
+	await expect(page.getByText('2 adults, 1 children')).toBeVisible()
+	await expect(page.getByText('3 Star Rating')).toBeVisible()
+
+	await expect(page.getByRole('link', { name: 'View Details' })).toBeVisible()
+	await expect(page.getByRole('link', { name: 'Add Hotel' })).toBeVisible()
+})
